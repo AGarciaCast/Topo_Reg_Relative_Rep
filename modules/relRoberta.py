0d9a47cce08d90c34256c7d7a855331bdf207035
AGarciaCast <al.garciacastellanos@gmail.com>
2023-04-13 17:36:57 +0000

    change code to better train

--- modules/relRoberta.py
+++ modules/relRoberta.py
@@ -3,31 +3,70 @@ import torch
 from torch import nn
 from typing import Optional, List
 from tqdm import tqdm
+import torch.nn.functional as F
 
 from modules.relAttention import RelativeAttention
 from transformers import RobertaModel, AutoConfig
 
 
+# +
+class Lambda(nn.Module):
+    def __init__(self, func):
+        super().__init__()
+        self.func = func
+
+    def forward(self, x):
+        return self.func(x)
+
 class RobertaClassificationHead(nn.Module):
     """
+    Modified from
     https://github.com/huggingface/transformers/blob/68287689f2f0d8b7063c400230b3766987abf18d/src/transformers/models/roberta/modeling_roberta.py#L1439
     Head for sentence-level classification tasks.
     """
 
     def __init__(self, hidden_size, num_labels, hidden_dropout_prob=0.1):
         super().__init__()
+        """
+        self.net = nn.Sequential(
+            nn.LayerNorm(normalized_shape=hidden_size),
+            nn.Linear(hidden_size, hidden_size),
+            nn.GELU(),
+            nn.Dropout(hidden_dropout_prob),
+            nn.Linear(hidden_size, hidden_size),
+            nn.GELU(),
+            nn.Dropout(hidden_dropout_prob),
+            nn.Linear(hidden_size, num_labels),
+            )
         
-        self.dense = nn.Linear(hidden_size, hidden_size)
-        self.dropout = nn.Dropout(hidden_dropout_prob)
-        self.out_proj = nn.Linear(hidden_size, num_labels)
+        """
+        self.net = nn.Sequential(
+        nn.LayerNorm(normalized_shape=hidden_size),
+        nn.Linear(in_features=hidden_size, out_features=hidden_size),
+        nn.SiLU(),
+        Lambda(lambda x: x.permute(1, 0)),
+        nn.InstanceNorm1d(num_features=hidden_size),
+        Lambda(lambda x: x.permute(1, 0)),
+        nn.Dropout(hidden_dropout_prob),
+        nn.Linear(in_features=hidden_size, out_features=hidden_size),
+        nn.SiLU(),
+        Lambda(lambda x: x.permute(1, 0)),
+        nn.InstanceNorm1d(num_features=hidden_size),
+        Lambda(lambda x: x.permute(1, 0)),
+        nn.Dropout(hidden_dropout_prob),
+        nn.Linear(
+            in_features=hidden_size, out_features=num_labels
+        )
+        )
+       
+       
 
     def forward(self, x, **kwargs):
-        x = self.dropout(x)
-        x = self.dense(x)
-        x = torch.tanh(x)
-        x = self.dropout(x)
-        x = self.out_proj(x)
+
+        x = self.net(x)
         return x
+# -
+
 
 
 class RelRoberta(nn.Module):
@@ -41,6 +80,7 @@ class RelRoberta(nn.Module):
         normalization_mode="l2",
         output_normalization_mode=None,
         dropout_prob=0.1,
+        freq_anchors=100,
         device="cpu"
     ) -> None:
         
@@ -51,12 +91,13 @@ class RelRoberta(nn.Module):
         configuration = AutoConfig.from_pretrained(transformer_model)
         configuration.hidden_dropout_prob = dropout_prob
         configuration.attention_probs_dropout_prob = dropout_prob
+        configuration.output_hidden_states=True
+        configuration.return_dict=True
             
         self.encoder = RobertaModel.from_pretrained(
                             pretrained_model_name_or_path = transformer_model, 
                             config = configuration,
-                            add_pooling_layer=False
-                        ).to(device)
+                        ).requires_grad_(False).eval()
 
         
         self.decoder = RobertaClassificationHead(
@@ -64,9 +105,8 @@ class RelRoberta(nn.Module):
             num_labels=num_labels,
             hidden_dropout_prob=dropout_prob
         )
-
-        self.anchors = None
-        self.anchors_latent = None
+        
+        self.device = device
 
         if anchor_dataloader is not None:
             self.relative_attention=RelativeAttention(
@@ -75,19 +115,11 @@ class RelRoberta(nn.Module):
                 normalization_mode=normalization_mode,
                 output_normalization_mode=output_normalization_mode
             )
-
-            anchors = []
-            anchors_latent = []
             
-            with torch.no_grad():
-                for batch in tqdm(anchor_dataloader, desc="Computing latents anchors"):
-                    anchors.append(batch)
-                    batch.to(device)
-                    batch_latents = self.embed(**batch)
-                    anchors_latent.append(batch_latents)
-                                
-            self.anchors = torch.cat(anchors, dim=0)
-            self.anchors_latent = torch.cat(anchors_latent, dim=0)
+            self.anchor_dataloader = anchor_dataloader
+            
+            self.freq_anchors = freq_anchors
+
       
 
     def embed(self,
@@ -117,7 +149,8 @@ class RelRoberta(nn.Module):
                               use_cache,
                               output_attentions,
                               output_hidden_states,
-                              return_dict)[0]
+                              return_dict)["hidden_states"][-1]
+        
         return result[:, 0, :]
     
     
@@ -134,7 +167,8 @@ class RelRoberta(nn.Module):
                use_cache: Optional[bool] = None,
                output_attentions: Optional[bool] = None,
                output_hidden_states: Optional[bool] = None,
-               return_dict: Optional[bool] = None): 
+               return_dict: Optional[bool] = None,
+               batch_idx=0): 
         
         x_embedded = self.embed(input_ids,
                                 attention_mask,
@@ -152,10 +186,21 @@ class RelRoberta(nn.Module):
         
         attention_encoding = {}
         
-        if self.anchors is not None:
+        if self.anchor_dataloader is not None:
+            
+            if batch_idx % self.freq_anchors == 0:
+                anchors_list = []
+                with torch.no_grad():
+                    for batch in self.anchor_dataloader:
+                        batch.to(self.device)
+                        batch_latents = self.embed(**batch)
+                        anchors_list.append(batch_latents)
+
+                self.anchors_latents = torch.cat(anchors_list, dim=0).to(self.device)
+                
             attention_encoding = self.relative_attention.encode(
                 x=x_embedded,
-                anchors=self.anchors_latent,
+                anchors=self.anchors_latents,
             )
             
         return {
@@ -167,7 +212,7 @@ class RelRoberta(nn.Module):
     def decode(self, batch_latent, **kwargs):
         
         attention_dict = {}
-        if self.anchors is not None:
+        if self.anchor_dataloader is not None:
             attention_output = self.relative_attention.decode(**kwargs)
             
             input_dec = attention_output["output"]
@@ -175,7 +220,7 @@ class RelRoberta(nn.Module):
                 "similarities": attention_output["similarities"],
                 "original_anchors": attention_output["original_anchors"],
                 "norm_anchors": attention_output["norm_anchors"],
-                "batch_latent": attention_output["batch_latent"],
+                "batch_latent": batch_latent,
                 "norm_batch": attention_output["norm_batch"]
             }
             
@@ -202,7 +247,8 @@ class RelRoberta(nn.Module):
                 use_cache: Optional[bool] = None,
                 output_attentions: Optional[bool] = None,
                 output_hidden_states: Optional[bool] = None,
-                return_dict: Optional[bool] = None): 
+                return_dict: Optional[bool] = None,
+                batch_idx=0): 
         """Forward pass."""
         
         encoding = self.encode(input_ids,
@@ -217,6 +263,7 @@ class RelRoberta(nn.Module):
                                use_cache,
                                output_attentions,
                                output_hidden_states,
-                               return_dict)
+                               return_dict,
+                               batch_idx)
         
         return self.decode(**encoding)
--- pl_modules/pl_roberta.py
+++ pl_modules/pl_roberta.py
@@ -2,11 +2,12 @@
 import torch.nn as nn
 # PyTorch Lightning
 import pytorch_lightning as pl
-from transformers import get_cosine_schedule_with_warmup, AdamW
+import torch.optim as optim
 from modules.relRoberta import RelRoberta
+import transformers
 
 
-def roberta_base_AdamW_LLRD(model, init_lr=1e-3, head_lr=1e-3, layer_decay=0.65, weight_decay=0.01):
+def roberta_base_AdamW_LLRD(model, encoder_lr=3.6e-6, head_lr=1e-3, layer_decay=0.65, weight_decay=0.01):
     """
     https://towardsdatascience.com/advanced-techniques-for-fine-tuning-transformers-82e4e61e16e
     """
@@ -17,15 +18,13 @@ def roberta_base_AdamW_LLRD(model, init_lr=1e-3, head_lr=1e-3, layer_decay=0.65,
     # According to AAAMLP book by A. Thakur, we generally do not use any decay 
     # for bias and LayerNorm.weight layers.
     no_decay = ["bias", "LayerNorm.bias", "LayerNorm.weight"]
-    init_lr = 1e-3 
-    head_lr = 1e-3
-    lr = init_lr
+    lr = encoder_lr
     
     # === Pooler and regressor ======================================================  
     
-    params_0 = [p for n,p in named_parameters if ("pooler" in n or "regressor" in n) 
+    params_0 = [p for n,p in named_parameters if ("decoder" in n) 
                 and any(nd in n for nd in no_decay)]
-    params_1 = [p for n,p in named_parameters if ("pooler" in n or "regressor" in n)
+    params_1 = [p for n,p in named_parameters if ("decoder" in n)
                 and not any(nd in n for nd in no_decay)]
     
     head_params = {"params": params_0, "lr": head_lr, "weight_decay": 0.0}    
@@ -63,7 +62,7 @@ def roberta_base_AdamW_LLRD(model, init_lr=1e-3, head_lr=1e-3, layer_decay=0.65,
     embed_params = {"params": params_1, "lr": lr, "weight_decay": weight_decay} 
     opt_parameters.append(embed_params)        
     
-    return AdamW(opt_parameters, lr=init_lr)
+    return optim.AdamW(opt_parameters, lr=head_lr)
 
 
 class LitRelRoberta(pl.LightningModule):
@@ -80,9 +79,11 @@ class LitRelRoberta(pl.LightningModule):
                  seed=42,
                  epochs=20,
                  weight_decay=0.0,
-                 lr_init=1e-3,
+                 head_lr=1e-3,
+                 encoder_lr=3.6e-6,
                  layer_decay=0.65,
-                 warmup_steps=10,
+                 scheduler_act=True,
+                 freq_anchors=100,
                  device="cpu"):
         super().__init__()
         
@@ -100,20 +101,23 @@ class LitRelRoberta(pl.LightningModule):
                               normalization_mode,
                               output_normalization_mode,
                               dropout_prob,
+                              freq_anchors,
                               device=device)
                 
         self.loss_module = nn.CrossEntropyLoss()
+        self.aux_loss = nn.L1Loss()
         
-        self.warmup_steps = warmup_steps
+        self.scheduler_act = scheduler_act
         self.epochs = epochs
         self.layer_decay = layer_decay
         self.weight_decay = weight_decay
-        self.lr_init = lr_init
+        self.head_lr = head_lr
+        self.encoder_lr = encoder_lr
         
         
     def forward(self, x):
        
-        return self.net(x)["prediction"]
+        return self.net(**x)["prediction"]
         
     def detailed_forward(self, x):
         """
@@ -124,48 +128,62 @@ class LitRelRoberta(pl.LightningModule):
     
     def configure_optimizers(self):
         config = {"optimizer": roberta_base_AdamW_LLRD(self.net,
-                                            init_lr=self.lr_init,
-                                            head_lr=self.lr_init,
+                                            encoder_lr=self.encoder_lr,
+                                            head_lr=self.head_lr,
                                             layer_decay=self.layer_decay,
                                             weight_decay=self.weight_decay
                                             )
                   }
+        
+        if self.scheduler_act:
+            config["lr_scheduler"] = {
+            "scheduler": transformers.get_scheduler(
+                                                    "linear",    
+                                                    optimizer = config["optimizer"],
+                                                    num_warmup_steps=int(self.epochs*0.1),
+                                                    num_training_steps = self.epochs
+                                                    ),
+                "interval": "step",
 
-        if self.warmup_steps is not None:
-          config["scheduler"] = get_cosine_schedule_with_warmup(
-                                      config["optimizer"],
-                                      num_warmup_steps=self.warmup_steps,
-                                      num_training_steps=self.epochs
-                                      )
+            }
         
         
-        return config
+        return {"optimizer": optim.Adam(self.net.parameters(), lr=1e-3)}
     
     def training_step(self, batch, batch_idx):
         # "batch" is the output of the training data loader.
         tokens, labels = batch
-        preds = self.net(**tokens)["prediction"]
+        preds = self.net(batch_idx=batch_idx, **tokens)["prediction"]
         loss = self.loss_module(preds, labels)
-        acc = (preds.argmax(dim=-1) == labels).float().mean()
+        prediction = preds.argmax(dim=-1)
+        acc = (prediction == labels).float().mean()
+        mae = self.aux_loss(prediction.float(), labels.float())*100
 
         # Logs the accuracy per epoch to tensorboard (weighted average over batches)
         self.log("train_acc", acc, prog_bar=True)
-        self.log("train_loss", loss, prog_bar=True)
+        self.log("train_loss", loss)
+        self.log("train_mae", mae, prog_bar=True)
         return loss  # Return tensor to call ".backward" on
 
     def validation_step(self, batch, batch_idx):
         tokens, labels = batch
-        preds = self.net(**tokens)["prediction"]
+        preds = self.net(batch_idx=batch_idx, **tokens)["prediction"]
         loss = self.loss_module(preds, labels)
-        acc = (preds.argmax(dim=-1) == labels).float().mean()
+        prediction = preds.argmax(dim=-1)
+        acc = (prediction == labels).float().mean()
+        mae = self.aux_loss(prediction.float(), labels.float())*100
+
 
         # Logs the accuracy per epoch to tensorboard (weighted average over batches)
         self.log("val_acc", acc, prog_bar=True)
+        self.log("val_mae", mae, prog_bar=True)
         self.log("val_loss", loss)
 
     def test_step(self, batch, batch_idx):
         tokens, labels = batch
-        preds = self.net(**tokens)["prediction"].argmax(dim=-1)
+        preds = self.net(batch_idx=batch_idx, **tokens)["prediction"].argmax(dim=-1)
         acc = (labels == preds).float().mean()
         # By default logs it per epoch (weighted average over batches), and returns it afterwards
         self.log("test_acc", acc)
+
+
