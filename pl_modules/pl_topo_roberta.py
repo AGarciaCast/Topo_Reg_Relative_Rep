7af5df430068e439ae5014b47463c32e3209c5b3
AGarciaCast <al.garciacastellanos@gmail.com>
2023-05-29 18:22:40 +0000

    trying L inf

--- pl_modules/pl_topo_roberta.py
+++ pl_modules/pl_topo_roberta.py
@@ -143,7 +143,7 @@ class LitTopoRelRoberta(pl.LightningModule):
             if self.net.anchor_dataloader is None:
                 assert self.latent_pos == "pre"
             if self.latent_pos == "both":
-                self.reg_loss = (TopoRegLoss(top_scale=topo_par[2],
+                self.reg_loss = (TopoRegLoss(top_scale=3,
                                             pers="L_2",
                                             loss_type=topo_par[4]
                                            ),
